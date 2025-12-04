4d8bf7d7a76e6da1359ebef4eae55e85326979d3
Quentin <buiquentinmacduyrgmail.com>
2025-12-04 22:16:10 +0100

    premier commit

---
+++ Chess/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Chess.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ Chess/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Chess.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
